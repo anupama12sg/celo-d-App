bfe5a748541e6da88c08cf4782eca8d22a01377b
Anu <59641189+anupama12sg@users.noreply.github.com>
2023-04-13 13:51:51 +0530

    Improvements-
    - Added relevant comments to the code.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
+// Import necessary testing libraries
 import { render, screen } from '@testing-library/react';
+
+// Import the component to be tested
 import App from './App';
 
+// Define a test case
 test('renders learn react link', () => {
+  // Render the App component
   render(<App />);
+
+  // Get the element that contains the text "learn react", ignoring case
   const linkElement = screen.getByText(/learn react/i);
+
+  // Assert that the element is present in the rendered output
   expect(linkElement).toBeInTheDocument();
 });
