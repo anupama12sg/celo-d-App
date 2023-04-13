766e2280c84ec1a4695ad74f1beafb9aa8644940
Anu <59641189+anupama12sg@users.noreply.github.com>
2023-04-13 13:53:59 +0530

    Improvements-
    - Added relevant comments to the code for better understanding and knowledge.

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,23 @@
+// Define a function called reportWebVitals which takes a callback function onPerfEntry as an argument
 const reportWebVitals = onPerfEntry => {
+  // Check if the onPerfEntry is a function
   if (onPerfEntry && onPerfEntry instanceof Function) {
+    // Dynamically import the 'web-vitals' library and then call the various functions from it
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      // Call the getCLS function with the onPerfEntry callback
       getCLS(onPerfEntry);
+      // Call the getFID function with the onPerfEntry callback
       getFID(onPerfEntry);
+      // Call the getFCP function with the onPerfEntry callback
       getFCP(onPerfEntry);
+      // Call the getLCP function with the onPerfEntry callback
       getLCP(onPerfEntry);
+      // Call the getTTFB function with the onPerfEntry callback
       getTTFB(onPerfEntry);
     });
   }
 };
 
+// Export the reportWebVitals function as the default export
 export default reportWebVitals;
+
